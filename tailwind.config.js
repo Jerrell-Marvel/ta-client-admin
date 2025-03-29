dc42f4c1fc1eebc4c7787f18632e135a45cc8135
Jerrell Marvel <jerrell.marvel88@gmail.com>
2025-03-29 12:55:45 +0700

    create login page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
-import {heroui} from "@heroui/theme"
+import { heroui } from "@heroui/theme";
 
 /** @type {import('tailwindcss').Config} */
 const config = {
-  content: [
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"
-  ],
+  content: ["./components/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}", "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
@@ -17,6 +13,6 @@ const config = {
   },
   darkMode: "class",
   plugins: [heroui()],
-}
+};
 
 module.exports = config;

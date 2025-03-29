c9b581ab051efa9fee2b8386c72a844e2fd751d0
Jerrell Marvel <jerrell.marvel88@gmail.com>
2025-03-29 12:23:16 +0700

    initial setup

---
+++ app/layout.tsx
@@ -0,0 +1,37 @@
+import "@/styles/globals.css";
+import { Metadata, Viewport } from "next";
+
+import { Providers } from "./providers";
+
+import { siteConfig } from "@/config/site";
+import { fontSans } from "@/config/fonts";
+import { Button } from "@heroui/button";
+
+export const metadata: Metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html
+      suppressHydrationWarning
+      lang="en"
+    >
+      <head />
+      <body className={`min-h-screen bg-background font-sans antialiased ${fontSans.variable} light`}>
+        <Providers themeProps={{ attribute: "class", defaultTheme: "light", enableSystem: false }}>
+          <div>Hello</div>
+          <Button>YES</Button>
+          {children}
+        </Providers>
+      </body>
+    </html>
+  );
+}
